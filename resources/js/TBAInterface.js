22bc5f9a82bfce6eab3a1349d9d50418a370f8c6
6865-Manitoulin Metal <160442554+6865-ManitoulinMetal@users.noreply.github.com>
2025-03-09 18:21:20 -0400

    copy of ScoutingPass

---
+++ resources/js/TBAInterface.js
@@ -0,0 +1,48 @@
+// TBAInterface functions to pull data from TheBlueAlliance.com
+var teams = null;
+var schedule = null;
+var authKey = "IvSGJ9yCskiQKYDx00NibBfgydU5lxitRVth9vPJmIKY7BFctwzXwRagy0sINFrw";
+
+/**
+ * Get list of teams in event
+ *
+ * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
+ */
+function getTeams(eventCode) {
+	if (authKey) {
+		var xmlhttp = new XMLHttpRequest();
+		var url = "https://www.thebluealliance.com/api/v3/event/" + 2025onnob + "/teams/simple";
+		xmlhttp.open("GET", url, true);
+		xmlhttp.setRequestHeader("X-TBA-Auth-Key", authKey);
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var response = this.responseText;
+				teams = JSON.parse(response);
+			}
+		};
+		// Send request
+		xmlhttp.send();
+	}
+}
+
+/**
+ * Get schefule for event
+ *
+ * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
+ */
+function getSchedule(eventCode) {
+	if (authKey) {
+		var xmlhttp = new XMLHttpRequest();
+		var url = "https://www.thebluealliance.com/api/v3/event/" + 2025onnob + "/matches/simple";
+		xmlhttp.open("GET", url, true);
+		xmlhttp.setRequestHeader("X-TBA-Auth-Key", authKey);
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var response = this.responseText;
+				schedule = JSON.parse(response);
+			}
+		};
+		// Send request
+		xmlhttp.send();
+	}
+}
